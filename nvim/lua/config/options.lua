2cc6029ecc780720032578e32b8a7c602c2dc421
WayanLu <ryliu3@gmail.com>
2025-03-16 19:34:45 -0700

    added centered cursor during file motions

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -40,9 +40,8 @@ opt.splitbelow = true -- split horizontal window to the bottom
 -- turn off swapfile
 opt.swapfile = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-
+opt.scrolloff = 60
